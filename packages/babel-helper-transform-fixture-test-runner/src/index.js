f78ef65ea1e797c85541329b0db6aef2971e7cfd
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-02-12 21:15:06 +0100

    Use require.resolve instead of the "resolve" package (#11125)
    * Remove resolve from package.json
    
    * Add flow typings for require.resolve
    
    * Run codemod
    
    * Remove other usages
    
    * Update tests

--- packages/babel-helper-transform-fixture-test-runner/src/index.js
+++ packages/babel-helper-transform-fixture-test-runner/src/index.js
@@ -8,7 +8,6 @@ import defaults from "lodash/defaults";
 import escapeRegExp from "lodash/escapeRegExp";
 import * as helpers from "./helpers";
 import merge from "lodash/merge";
-import resolve from "resolve";
 import assert from "assert";
 import fs from "fs";
 import path from "path";
@@ -108,8 +107,8 @@ function runModuleInTestContext(
   context: Context,
   moduleCache: Object,
 ) {
-  const filename = resolve.sync(id, {
-    basedir: path.dirname(relativeFilename),
+  const filename = require.resolve(id, {
+    paths: [path.dirname(relativeFilename)],
   });
 
   // Expose Node-internal modules if the tests want them. Note, this will not execute inside
